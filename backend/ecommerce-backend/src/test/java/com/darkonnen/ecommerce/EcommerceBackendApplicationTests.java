ce73ef776e6b92db841fd50760a07907d7be925f
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-10-26 14:01:41 -0300

    creating the backend of an ecommerccommommerce webapp

---
+++ backend/ecommerce-backend/src/test/java/com/darkonnen/ecommerce/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkonnen.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
